ef531ee0b5f9579597656276213ca18d336e943c
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-06-20 12:32:23 +0700

    add comp nopage(pagenotfound) if route not define

--- src/component/NoPage.jsx
+++ src/component/NoPage.jsx
@@ -1,5 +1,11 @@
 const NoPage = () => {
-  return <div className="flex-1">404 PAGE NOT FOUND</div>;
+  return (
+    <div className="flex-1">
+      <div className="h-full w-full flex items-center justify-center font-bold text-xl">
+        404 PAGE NOT FOUND
+      </div>
+    </div>
+  );
 };
 
 export default NoPage;
