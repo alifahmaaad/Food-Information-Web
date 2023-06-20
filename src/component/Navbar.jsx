846e6de4980ab3f691fcff69e234b93c7ba8c613
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-06-20 12:38:41 +0700

    fix error in navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,17 +12,12 @@ const NavbarComp = () => {
         <button onClick={handleOpenNav}>
           <svg
             xmlns="<http://www.w3.org/2000/svg>"
-            class="h-6 w-6 cursor-pointer md:hidden block"
+            className="h-6 w-6 cursor-pointer md:hidden block"
             fill="white"
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
+            <path d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </button>
       </div>
